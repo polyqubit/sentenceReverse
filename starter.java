13dce0374a5161253cea6a6adec0d441b84995e6
polyqubit <polyumbrail@gmail.com>
2021-11-30 12:19:44 -0800

    concatenceation

--- starter.java
+++ starter.java
@@ -1,9 +1,21 @@
 import java.util.Scanner;
 
 public class starter {
-
+	
 	public static void main(String args[]) {
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a group of words, separated by ");
+		String inp = sc.nextLine();
+		//extract individual word blocks
+		System.out.println(inp);
+		int L = inp.lastIndexOf(" "); //last index of whitespace
+		inp = inp.trim();
+		boolean check = false;
+		while(!check) {
+			String block = inp.substring(L);
+			inp = inp.substring(L+1);
+			
+		}
 	}
 	
 }
