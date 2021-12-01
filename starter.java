d0688810c4a10e38ae6f72f4e8fc4bd268f989db
polyqubit <89955098+polyqubit@users.noreply.github.com>
2021-11-30 21:37:10 -0800

    begin process

--- starter.java
+++ starter.java
@@ -4,18 +4,22 @@ public class starter {
 	
 	public static void main(String args[]) {
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a group of words, separated by ");
+		System.out.println("Enter a group of words, separated by a space");
 		String inp = sc.nextLine();
-		//extract individual word blocks
-		System.out.println(inp);
-		int L = inp.lastIndexOf(" "); //last index of whitespace
+		System.out.println("\n"+inp+"\n");
 		inp = inp.trim();
-		boolean check = false;
-		while(!check) {
+		int check = 0;
+   		 char store = ' ';
+    		for(int i=0;i<inp.length();i++) {
+        		if(inp.charAt(i)==' ') {check++;}
+    		}
+		for(int i=0;i<check-1;i++) {
+      			int L = inp.lastIndexOf(" "); //last index of whitespace
+      			System.out.println(L);
 			String block = inp.substring(L);
-			inp = inp.substring(L+1);
-			
+			inp = inp.substring(0,L) + block;
 		}
+    		System.out.println(inp);
 	}
 	
 }
